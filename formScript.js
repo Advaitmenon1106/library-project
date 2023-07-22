bce91346c1abf0eea39a8f4b2f73b34a6d150245
advait0603 <advaitlinux11@gmail.com>
2023-07-22 10:16:23 +0530

    Basic logic implemented using JS. UI is left, the constructor instance needs to be added to an array which isn't happening and needs to be fixed.

---
+++ formScript.js
@@ -0,0 +1,56 @@
+function Book(title, author, readingStatus){
+    this.title = title;
+    this.author = author;
+    this.readingStatus = readingStatus;
+}
+
+let submitButton = document.getElementById('submitButton');
+let bookTitleField = document.getElementById('title');
+let authorNameField = document.getElementById('authorName');
+let radioButtons = document.querySelectorAll('input[name="readOrNot"]');
+let form = document.getElementById('form');
+let bookArray = localStorage.getItem('bookArray');
+
+submitButton.addEventListener('click', ()=>{
+    let book = new Book();
+    parent.document.scripts.namedItem
+    localStorage.setItem('bookName', bookTitleField.value);
+    localStorage.setItem('authorName', authorNameField.value);
+    book.title = localStorage.getItem('bookName');
+    book.author = localStorage.getItem('authorName');
+    
+    for (const radioButton of radioButtons){
+        if (radioButton.checked){
+            selection = radioButton.value;
+            break;
+        }
+    }
+    if (selection == 'Yes'){
+        localStorage.setItem('readOrNot', true);
+    }
+    else{
+        localStorage.setItem('readOrNot', false);
+    }
+
+    book.readingStatus = localStorage.getItem('readOrNot');
+
+    bookArray.length = book;
+    console.log(bookArray);
+
+    let shelf = parent.document.getElementById('shelf');
+    let shelfOuterBox = parent.document.getElementById('shelfOuterBox');
+    
+    let newBook = document.createElement('div');
+        newBook.className = 'books';
+        shelf.appendChild(newBook);
+
+        let bookTitle = document.createElement('div');
+        bookTitle.className = 'bookName';
+        newBook.appendChild(bookTitle);
+        bookTitle.innerHTML = localStorage.getItem('bookName');
+
+        let authorName = document.createElement('div');
+        authorName.className = 'authorName';
+        newBook.appendChild(authorName);
+        authorName.innerHTML = localStorage.getItem('authorName');
+})
---
+++ script.js
@@ -0,0 +1,32 @@
+let bookList = [];
+function Book(title, author, readingStatus){
+    this.title = title;
+    this.author = author;
+    this.readingStatus = readingStatus;
+}
+
+let books = document.getElementsByClassName('books');
+for (i = 0; i<books.length; i++){
+    bookList.push(new Book(books[i].childNodes[0].innerHTML, books[i].childNodes[2].innerHTML, true));
+}
+
+localStorage.setItem('bookArray', bookList);
+
+let newBookButton = document.getElementById('newButton');
+let shelf = document.getElementById('shelf');
+let shelfOuterBox = document.getElementById('shelfOuterBox');
+
+newBookButton.addEventListener('click', ()=>{
+    //window.location.href = 'form.html'; //Find a way to display this form on the main page instead of window.open()
+
+    let formBox = document.createElement('div')
+    shelfOuterBox.appendChild(formBox);
+
+    let formIframe = document.createElement('iframe');
+    formIframe.style.width = '500px';
+    formIframe.style.height = '300px';
+    formIframe.style.marginTop = '2%';
+    formIframe.style.border='none';
+    formIframe.src = 'form.html';
+    formBox.appendChild(formIframe);
+})
